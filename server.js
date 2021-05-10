0c15f83996679630a7a4c6ab5a12ae698f66f08f
cvsschaitanya <cvsschaitanya@gmail.com>
2021-05-11 00:15:04 +0530

    added password verification

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const express = require("express");
-// const theCookieParser = require("cookie-parser");
-// const ParseCookieTool = require("cookie-parse");
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const ExpressSession = require("express-session");
 const memstore = new ExpressSession.MemoryStore();
 const session = ExpressSession({
@@ -52,12 +54,94 @@ router.get("/signout", function (req, res) {
 	});
 });
 
-router.post("/chat", function (req, res) {
+router.post("/signin", function (req, res) {
 	username = req.body["username"];
+	password = req.body["password"];
 
-	req.session.username = username;
-	res.send("OK cool!");
-	// res.redirect("/chat");
+	db.serialize(() => {
+		db.all(
+			"SELECT * FROM _USERS WHERE username = ?;",
+			[username],
+			(error, rows) => {
+				if (error != null) console.log("Error:" + error);
+				else if (rows.length == 0) {
+					res.status(404);
+					res.send("No such user");
+				} else {
+					bcrypt.compare(
+						password,
+						rows[0]["password"],
+						function (err, result) {
+							if (err) {
+								console.log("Password Hashing Check Error:");
+								console.log(err);
+								return;
+							}
+
+							if (result) {
+								req.session.username = username;
+								res.send("OK cool!");
+								// res.redirect("/chat");
+							} else {
+								res.status(403);
+								res.send("Wrong password");
+							}
+						}
+					);
+				}
+			}
+		);
+	});
+});
+
+router.post("/register", function (req, res) {
+	username = req.body["username"];
+	password = req.body["password"];
+
+	db.serialize(() => {
+		db.all(
+			"SELECT * FROM _USERS WHERE username = ?;",
+			[username],
+			(error, rows) => {
+				if (error != null) console.log("Error:" + error);
+				else if (rows.length > 0) {
+					res.status(409);
+					res.send("Username exists");
+				} else {
+					req.session.username = username;
+
+					bcrypt.hash(
+						password,
+						saltRounds,
+						function (err, passwordhash) {
+							if (err) {
+								console.log("Password Hashing Error:");
+								console.log(err);
+								return;
+							}
+							db.serialize(() => {
+								db.run(
+									"INSERT INTO _USERS values (? , ? );",
+									[username, passwordhash],
+									(err) => {
+										if (err) {
+											console.error(err.message);
+										} else {
+											console.log(
+												`New user ${username} added.`
+											);
+										}
+									}
+								);
+							});
+							res.send("OK cool!");
+							// res.redirect("/chat");
+						}
+					);
+				}
+			}
+		);
+	});
 });
 
 // app.use(theCookieParser());
--- temp.sql
+++ temp.sql
@@ -1,1 +1,8 @@
-select * from chats;
+-- create table _USERS(
+--     username VARCHAR(255) NOT NULL PRIMARY KEY,
+--     password VARCHAR(255) NOT NULL
+-- );
+
+select * from _USERS;
+-- INSERT into _USERS values ('aaa','aaa');
+
